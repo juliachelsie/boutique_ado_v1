084af48365cd6bccc04ff7cb7a8ced00d685b83b
juliachelsie <>
2023-12-20 14:12:52 +0000

    Deployment part 5 retry

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,7 +181,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'botiqueado-julia'
+    AWS_STORAGE_BUCKET_NAME = 'boutiqueado-julia'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
